358c55488cd9a4ca7a275c1e0822a3e46d87b8fc
Danila <dakfadeev@gmail.com>
2021-10-23 20:16:53 +0300

    new models + ready conveyor

--- Assets/Scripts/СonveyorTrigger.cs
+++ Assets/Scripts/СonveyorTrigger.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class СonveyorTrigger : MonoBehaviour
 {
+    [SerializeField] float speed = 2;
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Letter")
         {
-            other.gameObject.transform.position += Vector3.right * Time.deltaTime;
+            other.gameObject.transform.position += Vector3.right * Time.deltaTime * speed;
         }
     }
 }
