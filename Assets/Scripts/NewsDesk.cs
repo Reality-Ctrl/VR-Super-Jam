3e2bbe110e5555827e318630f20e8a9ef85d4324
Danila <dakfadeev@gmail.com>
2021-10-24 15:37:50 +0300

    news desc

--- Assets/Scripts/NewsDesk.cs
+++ Assets/Scripts/NewsDesk.cs
@@ -2,13 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using UnityEngine;
+using TMPro;
 
 public class NewsDesk : MonoBehaviour
 {
-    private Stack<string> news = new Stack<string>();
+    [SerializeField] private Stack<string> news = new Stack<string>();
+    [SerializeField] TMP_Text news_desc;
 
     public void AddNewsOnNextDay(string news)
     {
         this.news.Push(news);
+        AddNewsToDesk(news);
+    }
+
+    void AddNewsToDesk(string news)
+    {
+        news_desc.text += "\n" + news;
     }
 }
