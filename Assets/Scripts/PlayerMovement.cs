e9709c1666613ab855075565e49f4aa839a9b0f7
Mixail <mixail.korchikov@mail.ru>
2021-10-29 21:01:26 +0300

    Fix snap turn
    close #23

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,4 +1,3 @@
-using System.Diagnostics.Contracts;
 using UnityEngine;
 using Valve.VR;
 using Valve.VR.InteractionSystem;
@@ -7,21 +6,41 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 0.1f;
     [SerializeField] private SteamVR_Action_Vector2 joystickInput;
+    [SerializeField] private SteamVR_Action_Boolean turnLeft;
+    [SerializeField] private SteamVR_Action_Boolean turnRight;
     [SerializeField] private Transform camera;
+    [SerializeField] private RotationAngles rotationAngle = RotationAngles.Forty_five;
     private CharacterController characterController;
 
     private void Awake()
     {
         characterController = gameObject.GetComponent<CharacterController>();
+        float angle = (float) rotationAngle;
+        turnLeft.onStateUp += (action, source) => Rotate(-angle);
+        turnRight.onStateUp += (action, source) => Rotate(angle);
     }
 
     private void FixedUpdate()
     {
+        #region CharController
         characterController.height = Player.instance.eyeHeight;
         float distToFloor = Vector3.Dot(camera.localPosition, Vector3.up);
         characterController.center = camera.localPosition - 0.5f * distToFloor * Vector3.up;
+        #endregion
 
         Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(joystickInput.axis.x, 0, joystickInput.axis.y));
         characterController.Move(Vector3.ProjectOnPlane(direction, Vector3.up) * speed - new Vector3(0, 10, 0));
     }
+
+    private void Rotate(float angle)
+    {
+        transform.Rotate(0, angle, 0);
+    }
+}
+
+enum RotationAngles
+{
+    Thirty = 30,
+    Forty_five = 45,
+    Ninety = 90
 }
