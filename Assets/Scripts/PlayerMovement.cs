e5224a87ff3970dab20daa33722e96c85debd810
Mixail <mixail.korchikov@mail.ru>
2021-11-08 15:34:42 +0300

    Fix player rotatoin

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,10 +11,12 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Transform camera;
     [SerializeField] private RotationAngles rotationAngle = RotationAngles.Forty_five;
     private CharacterController characterController;
+    private Player player;
 
     private void Awake()
     {
         characterController = gameObject.GetComponent<CharacterController>();
+        player = Player.instance;
         float angle = (float) rotationAngle;
         turnLeft.onStateUp += (action, source) => Rotate(-angle);
         turnRight.onStateUp += (action, source) => Rotate(angle);
@@ -23,18 +25,22 @@ public class PlayerMovement : MonoBehaviour
     private void FixedUpdate()
     {
         #region CharController
-        characterController.height = Player.instance.eyeHeight;
+        characterController.height = player.eyeHeight;
         float distToFloor = Vector3.Dot(camera.localPosition, Vector3.up);
         characterController.center = camera.localPosition - 0.5f * distToFloor * Vector3.up;
         #endregion
 
-        Vector3 direction = Player.instance.hmdTransform.TransformDirection(new Vector3(joystickInput.axis.x, 0, joystickInput.axis.y));
+        Vector3 direction = player.hmdTransform.TransformDirection(new Vector3(joystickInput.axis.x, 0, joystickInput.axis.y));
         characterController.Move(Vector3.ProjectOnPlane(direction, Vector3.up) * speed - new Vector3(0, 10, 0));
     }
 
     private void Rotate(float angle)
     {
-        transform.Rotate(0, angle, 0);
+        Vector3 playerFeetOffset = player.trackingOriginTransform.position - player.feetPositionGuess;
+        player.trackingOriginTransform.position -= playerFeetOffset;
+        player.transform.Rotate(Vector3.up, angle);
+        playerFeetOffset = Quaternion.Euler(0.0f, angle, 0.0f) * playerFeetOffset;
+        player.trackingOriginTransform.position += playerFeetOffset;
     }
 }
 
